d1463e5439aca9e898b633256847b4394f1bf58c
Gideon <eignnx@gmail.com>
2024-01-20 14:06:57 -0500

    factor out cli, make lib

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+// use clap::Parser;
+
+// use cpu::{Cpu, MemBlock, MemRw, Memory};
+
+pub mod cpu;
+pub mod log;
+pub mod utils;
+
+// fn main() {
+
+// let cli = cli::Cli::parse();
+
+// let vec = std::fs::read(&cli.romfile).expect("Failed to read ROM file");
+// let size = vec.len();
+// let Some(rom) = MemBlock::from_vec(vec) else {
+//     eprintln!("ROM file is too large:");
+//     eprintln!(
+//         "\tFile `{}` requires {} bytes. ROM has only {} bytes.",
+//         cli.romfile.display(),
+//         size,
+//         cpu::ROM_SIZE,
+//     );
+//     std::process::exit(1);
+// };
+
+// let mut cpu = Cpu::new(rom)
+//     .with_start_addr(Memory::ROM_START)
+//     .in_debug_mode(cli.debug)
+//     .with_rom_src_path(cli.rom_src_path());
+
+// if cli.print_rom {
+//     for i in Memory::ROM_START..Memory::ROM_START + size as u16 {
+//         let byte = cpu.mem.read_u8(i);
+//         print!("{:02X} ", byte);
+//         if i % 16 == 15 {
+//             println!();
+//         }
+//     }
+//     println!();
+// }
+
+// cpu.run();
+// }
