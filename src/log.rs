20e7fd8389a2198165f1d90be48ebf2470cb96ef
Gideon <eignnx@gmail.com>
2024-01-21 01:37:52 -0500

    Refactor logging and add reset functionality

--- src/log.rs
+++ src/log.rs
@@ -1,48 +1,45 @@
 #[macro_export]
 macro_rules! log {
     ($($arg:tt)*) => {
-        if cfg!(debug_assertions) {
-            print!("[LOG]: ");
-            println!($($arg)*);
-        }
+        print!("[LOG]: ");
+        println!($($arg)*);
     };
 }
 
 #[macro_export]
 macro_rules! log_instr {
     ([$size:expr] $name:ident) => {
-        if cfg!(debug_assertions) {
-            print!("[LOG]: ");
-            print!("|{}|", $size);
-            print!("\t{}", stringify!($name));
-            println!();
+        $crate::cpu::LogMsg::Instr {
+            size: $size,
+            name: stringify!($name).to_string(),
+            args: vec![]
         }
     };
     ([$size:expr] $name:ident $firstargval:expr $(, $argval:expr)*) => {
-        if cfg!(debug_assertions) {
-            use yansi::Paint;
-            print!("[LOG]: ");
-            print!("|{}|", $size);
-            print!("\t{}\t", stringify!($name).yellow().bold());
-            match stringify!($firstargval) {
-                "rd" | "rs" | "rt" => {
-                    print!("{}", $firstargval.to_string().magenta());
+        $crate::cpu::LogMsg::Instr {
+            size: $size,
+            name: stringify!($name).to_string(),
+            args: vec![
+                match stringify!($firstargval) {
+                    "rd" | "rs" | "rt" => {
+                        (Some($crate::cpu::ArgStyle::Reg), $firstargval.to_string())
+                    },
+                    _ => {
+                        (None, format!("{}={}", stringify!($firstargval), $firstargval))
+                    }
                 },
-                _ => {
-                    print!("{}={}", stringify!($firstargval), $firstargval);
-                }
-            }
-            $(
-            match stringify!($argval) {
-                "rd" | "rs" | "rt" => {
-                    print!(", {}", $argval.to_string().magenta());
-                },
-                _ => {
-                    print!(", {}={}", stringify!($argval), $argval);
-                }
-            }
-            )*
-            println!();
+
+                $(
+                    match stringify!($argval) {
+                        "rd" | "rs" | "rt" => {
+                            (Some($crate::cpu::ArgStyle::Reg), $argval.to_string())
+                        },
+                        _ => {
+                            (None, format!("{}={}", stringify!($argval), $argval))
+                        }
+                    },
+                )*
+            ],
         }
     };
 }
